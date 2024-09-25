4b5cf4252e9c0b08d3182ff3a31f710870f854e7
ranishraj007 <ranishraj.rs@gmail.com>
2024-09-25 17:31:21 +0545

    rating changed

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,9 @@ import "./index.css";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    {/* <StarRating maxRating={5}/> */}
-    {/* <StarRating maxRating={10}/>
-    <StarRating /> */}
+    <StarRating maxRating={5} />
+    <StarRating maxRating={10} size={24} color="red" />
+    <StarRating />
+    <StarRating maxRating={20} size={50} color="pink" />
   </StrictMode>
 );
